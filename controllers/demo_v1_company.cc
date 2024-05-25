1b534a081b215807ee25bc17998374bbf95b7e0c
ilya <isershov@edu.hse.ru>
2024-05-25 23:09:34 +0300

    Update controller for client

--- controllers/demo_v1_company.cc
+++ controllers/demo_v1_company.cc
@@ -38,3 +38,64 @@ void company::get_list(const drogon::HttpRequestPtr &req, std::function<void(con
     resp->setBody(stream.str());
     callback(resp);
 }
+
+void company::get_product(const drogon::HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, std::string&& name) {
+    auto clientPtr = drogon::app().getDbClient();
+    name = "%" + name + "%";
+    auto f = clientPtr->execSqlAsyncFuture("SELECT product.name as product_name, types.name as type from"
+                                           " product join types on product.fk_type = types.id\n"
+                                           "where product.produced = TRUE AND product.name like $1;",
+                                           name);
+    Json::Value jsonArray;
+    try
+    {
+        auto result = f.get(); // Block until we get the result or catch the exception;
+        for (auto row : result)
+        {
+            Json::Value tempObject;
+            tempObject["name"] = row["product_name"].as<std::string>();
+            tempObject["type"] = row["type"].as<std::string>();
+            jsonArray.append(std::move(tempObject));
+        }
+    }
+    catch (const drogon::orm::DrogonDbException &e)
+    {
+        std::cerr << "error:" << e.base().what() << std::endl;
+    }
+    auto resp = HttpResponse::newHttpJsonResponse(jsonArray);
+    resp->setStatusCode(k200OK);
+    callback(resp);
+}
+
+
+void company::post_delivery(const drogon::HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+    auto jsonBody = *req->getJsonObject();
+    Json::Value response;
+    if(!(jsonBody.isMember("amount") && jsonBody.isMember("product")
+    && jsonBody.isMember("date") && jsonBody.isMember("client")))
+    {
+        response["message"] = "Invalid data";
+        auto resp = HttpResponse::newHttpJsonResponse(response);
+        callback(resp);
+        return;
+    }
+    auto clientPtr = drogon::app().getDbClient();
+    auto f = clientPtr->execSqlAsyncFuture("INSERT INTO delivery(fk_product, fk_client, amount, date) "
+                                           "VALUES((SELECT id from product where name = $1),"
+                                           " (SELECT id from client where name = $2), $3, $4 )",
+                                           jsonBody["product"].asString(),  jsonBody["client"].asString(),
+                                           jsonBody["amount"].asString(), jsonBody["date"].asString());
+    try
+    {
+        auto result = f.get(); // Block until we get the result or catch the exception;
+        response["message"] = "Success";
+    }
+    catch (const drogon::orm::DrogonDbException &e)
+    {
+        std::cerr << "error:" << e.base().what() << std::endl;
+        response["message"] = "Some wrong";
+    }
+    auto resp = HttpResponse::newHttpJsonResponse(response);
+    resp->setStatusCode(k200OK);
+    callback(resp);
+}
--- controllers/demo_v1_company.h
+++ controllers/demo_v1_company.h
@@ -19,9 +19,13 @@ class company : public drogon::HttpController<company>
     // ADD_METHOD_TO(company::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
     METHOD_ADD(company::get_list, "/list", Get);
     METHOD_ADD(company::hello, "/", Get);
+    METHOD_ADD(company::get_product, "/product/{name}", Get);
+    METHOD_ADD(company::post_delivery, "/delivery/init", Post);
     METHOD_LIST_END
     void get_list(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
     void hello(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
+    void get_product(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string&& name);
+    void post_delivery(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
     // void your_method_name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, double p1, int p2) const;
